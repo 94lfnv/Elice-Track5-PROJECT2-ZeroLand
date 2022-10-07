d11b98b9f7b5071e0a24c35a759cd188f54b8113
haleychoi <chldudgus617@gmail.com>
2022-10-07 19:15:24 +0900

    Add new files

---
+++ frontend/src/components/Pages/Introduction.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Introduction () {
+    return (
+        <div>
+            <p>introduction page test</p>
+        </div>
+    );
+}
+
+export default Introduction;
---
+++ frontend/src/components/Pages/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Welcome() {
+    return (
+      <div className="banner">
+        <section id="banner">
+          <div class="inner">
+            <h1><a href="/introduction">🌿</a></h1>
+            <h2>Team 7 Zero-waste Shop Map Service</h2>
+          </div>
+        </section>
+      </div>
+    );
+}
+
+export default Welcome;
