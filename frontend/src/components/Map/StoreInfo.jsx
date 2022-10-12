f318826a8ad37235094c81bdb46710be3a9a7e8d
haleychoi <chldudgus617@gmail.com>
2022-10-12 18:13:35 +0900

    Fix: 카카오맵 상 마커 표시 기능 구현

--- frontend/src/components/Map/StoreInfo.jsx
+++ frontend/src/components/Map/StoreInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function StoreInfo() {
   return (
-    <div className="inner">
+    <div class="inner" style={{float: "right"}}>
         <p>
         StoreInfo component test <br />
         <span class="image left"><img src="img/pic05.jpg" alt="가게 썸네일" /></span>가게 이름     별점 아이콘 (5/5) <br />
--- frontend/src/components/Pages/Map.jsx
+++ frontend/src/components/Pages/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEfeect } from "react"
+import React from "react"
 
 import Location from "../Map/Location";
 import StoreInfo from "../Map/StoreInfo";
@@ -6,13 +6,10 @@ import StoreInfo from "../Map/StoreInfo";
 function Map() {
   return (
     <article id="main">
-      {/* <header>
-        <h2>당신 주변의 제로웨이스트샵</h2>
-      </header> */}
       <section class="wrapper style5">
         <div class="inner">
           <div>
-            <h3>내 주변의 제로웨이스트샵</h3>
+          <h3>내 주변의 제로웨이스트샵</h3>
             <Location />
             <StoreInfo />
           </div>
