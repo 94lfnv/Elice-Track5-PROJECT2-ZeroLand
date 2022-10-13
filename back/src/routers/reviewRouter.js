f3ae205d4ad4aa271d2504dcca63f9b1d6e3eef9
박 은정 <scpark6021@gmail.com>
2022-10-13 10:38:52 +0900

    [feat]reveiwList get 완성

--- back/src/routers/reviewRouter.js
+++ back/src/routers/reviewRouter.js
@@ -4,41 +4,62 @@ const { reviewService } = require("../services/reviewService");
 
 const reviewRouter = express.Router();
 
-reviewRouter.post("/stores/:store_id/review", async (req, res, next) => {
-try {//로그인 돼있나 확인하는 코드 나중에 추가예정
-    //로그인된 유저를 토큰 값으로 확인한후 user_id를 받아오기
-    // const user_id = req.currentUserId;
-    const user_id = req.body.user_id;
-    const star = req.body.star;
-    const description = req.body.description;
-    const photo = req.body.photo;
-    const store_id = req.params.store_id
-    
-    // db에 저장
-    const [results, fields, error] = await pool.query(
-        {
-          sql: "INSERT INTO reviews (user_id, star, description, photo, store_id) VALUES (?, ?, ?, ?, ?)",
-          values: [user_id,
-            star,
-            description,
-            photo,
-            store_id,
-           ]
-        });
-    if (error) throw error;
-    // console.log(results);
-    // const newReview = JSON.stringify(results, ["insertId"]);
-    // console.log(newReview);
+const addReview = async (req, res, next) => {
+  try {//로그인 돼있나 확인하는 코드 나중에 추가예정
+      //로그인된 유저를 토큰 값으로 확인한후 user_id를 받아오기
+      // const user_id = req.currentUserId;
+      const user_id = req.body.user_id;
+      const star = req.body.star;
+      const description = req.body.description;
+      const photo = req.body.photo;
+      const store_id = req.params.store_id
+      
+      // db에 저장
+      const [results, fields, error] = await pool.query(
+          {
+            sql: "INSERT INTO reviews (user_id, star, description, photo, store_id) VALUES (?, ?, ?, ?, ?)",
+            values: [user_id,
+              star,
+              description,
+              photo,
+              store_id,
+            ]
+          });
+      if (error) throw error;
+      // console.log(results);
+      // const newReview = JSON.stringify(results, ["insertId"]);
+      // console.log(newReview);
 
-    console.log(results)
-    const review_id = results.insertId;
-    const saveData = { review_id, star, description, photo }
-    
-    res.status(201).send(saveData);
-    } catch (err) {
-    next(err);
-    }
-});
+      console.log(results)
+      const review_id = results.insertId;
+      const saveData = { review_id, star, description, photo }
+      
+      res.status(201).send(saveData);
+      } catch (err) {
+      next(err);
+      }
+  };
+
+const storeReviews = async (req, res, next) => {
+  try {
+    const store_id = req.params.store_id;
+    console.log(store_id)
+    const [results, fields, error] = await pool.query({
+      sql: "SELECT * FROM reviews WHERE store_id = ?",
+      values: [store_id]
+  });
+  if (error) throw error;
+  console.log(results);
+
+  res.status(201).json(results);
+  } catch (err) {
+  next(err);
+  }
+};
+
+//api index
+reviewRouter.post("/stores/:store_id/review", addReview)
+reviewRouter.get("/stores/:store_id/reviews", storeReviews)
 
 module.exports = reviewRouter;
 
