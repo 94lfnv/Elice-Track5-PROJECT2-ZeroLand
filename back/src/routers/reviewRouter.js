089eb8096c96bf74403148cead0391dee470e8f7
박 은정 <scpark6021@gmail.com>
2022-10-15 00:38:16 +0900

    [Feat]: review api photo빼고 구현.

--- back/src/routers/reviewRouter.js
+++ back/src/routers/reviewRouter.js
@@ -4,11 +4,11 @@ const { reviewService } = require("../services/reviewService");
 
 const reviewRouter = express.Router();
 
-const addReview = async (req, res, next) => {
+reviewRouter.post("/stores/:store_id/review", async (req, res, next) => {
   try {//로그인 돼있나 확인하는 코드 나중에 추가예정
       //로그인된 유저를 토큰 값으로 확인한후 user_id를 받아오기
       // const user_id = req.currentUserId;
-      const user_id = req.body.user_id;
+      const user_id = 3;
       const star = req.body.star;
       const description = req.body.description;
       const photo = req.body.photo;
@@ -26,9 +26,6 @@ const addReview = async (req, res, next) => {
             ]
           });
       if (error) throw error;
-      // console.log(results);
-      // const newReview = JSON.stringify(results, ["insertId"]);
-      // console.log(newReview);
 
       console.log(results)
       const review_id = results.insertId;
@@ -38,16 +35,15 @@ const addReview = async (req, res, next) => {
       } catch (err) {
       next(err);
       }
-  };
+  });
 
-const storeReviews = async (req, res, next) => {
+  reviewRouter.get("/stores/:store_id/reviews", async (req, res, next) => {
   try {
     const store_id = req.params.store_id;
-    console.log(store_id)
-    const [results, fields, error] = await pool.query({
-      sql: "SELECT * FROM reviews WHERE store_id = ?",
-      values: [store_id]
-  });
+    // console.log(store_id)
+    const [results, fields, error] = await pool.query(
+      `SELECT review_id, star, description, photo, created_time, updated_time FROM reviews WHERE store_id = ${store_id}`
+  );
   if (error) throw error;
   console.log(results);
 
@@ -55,11 +51,157 @@ const storeReviews = async (req, res, next) => {
   } catch (err) {
   next(err);
   }
-};
+});
+
+reviewRouter.get("/user/reviewList", async (req, res, next) => {
+  try {
+    // const user_id = req.currentUserId;
+    const user_id = 1
+  //   const [results, fields, error] = await pool.query(
+  //     `SELECT * FROM reviews JOIN like_reviews ON reviews.review_id = like_reviews.review_id WHERE reviews.user_id = ${user_id}`
+  // );
+  //   스칼라 서브쿼리 -> Operand should contain 1 column(s)
+  //   const [results, fields, error] = await pool.query(
+  //     `SELECT *, (SELECT * FROM like_reviews WHERE reviews.review_id = like_reviews.review_id) FROM reviews WHERE reviews.user_id = ${user_id}`
+  // );
+  // const [results, fields, error] = await pool.query(
+  //       `SELECT * FROM reviews JOIN like_reviews ON reviews.review_id = like_reviews.review_id WHERE reviews.user_id = ${user_id}`
+  //   );
+  
+  let [results, fields, error] = await pool.query(
+    `SELECT () FROM reviews WHERE reviews.user_id = ${user_id}`
+  );
+
+  // const reviews = JSON.stringify(results);
+
+  for (var i = 0; i < results.length; i++) {
+    let k = results[i].review_id
+    //그 리뷰를 좋아하는 사람 목록
+    let [reviewLikeList] = await pool.query(
+      `SELECT * FROM like_reviews WHERE like_reviews.review_id = ${k} and tag=1`)
+    //그 리뷰를 싫어하는 사람 목록
+    let [reviewDislikeList] = await pool.query(
+      `SELECT * FROM like_reviews WHERE like_reviews.review_id = ${k} and tag=2`)
+    results[i].like_reviews = reviewLikeList
+    results[i].dike_reviews = reviewDislikeList
+  }
+  console.log(results)
+
+  if (error) throw error;
+ 
+  res.status(201).json(results);
+  } catch (err) {
+  next(err);
+  }
+});
+
+reviewRouter.put("/review/:review_id", async (req, res, next) => {
+  try {
+    const review_id = req.params.review_id;
+    const {
+      star,
+      description,
+      photo
+    } = req.body;
+
+    const [results, fields, error] = await pool.query(
+      `UPDATE reviews SET star=${star}, description="${description}", photo="${photo}" WHERE review_id = ${review_id}`
+    );
+    const putData = { review_id, star, description, photo }
+
+    if (error) throw error;
+    res.status(201).send(putData);
+  } catch (err) {
+    next(err);
+  }
+});
+
+reviewRouter.delete("/stores/:store_id/:review_id", async (req, res, next) => {
+  try {
+    const review_id = req.params.review_id
+    const [results, fields, error] = await pool.query(
+      `DELETE FROM reviews WHERE review_id=${review_id};`
+    );
+    if (error) throw error;
+
+    res.status(201).json({
+         message: "리뷰가 삭제되었습니다."
+        });
+  } catch (err) {
+    next(err);
+  }
+});
+//예외처리 - 요청한 리뷰가 없을때도 잘삭제됐다고 나옴.
+
+
+//리뷰 라이크
+reviewRouter.post("/stores/:store_id/:review_id/like", async (req, res, next) => {
+  try {//로그인 돼있나 확인하는 코드 나중에 추가예정
+      //로그인된 유저를 토큰 값으로 확인한후 user_id를 받아오기
+      // const user_id = req.currentUserId;
+      const user_id = 2;
+      const review_id = req.params.review_id;
+      const tag = 1;
+
+      //db에 존재하는 것 삭제
+      const [results, fields, error] = await pool.query(
+        // `select count(user_id) from like_reviews group by review_id, having count(review_id)>1`
+        `DELETE FROM reviews WHERE review_id=${review_id} and user_id=${user_id}`
+      )
+      if (error) throw error;
+      // db에 저장
+      const [result, field, err] = await pool.query(
+        `INSERT INTO like_reviews user_id = ${user_id}, review_id = ${review_id}, tag = ${tag})`);
+      if (error) throw error;
+
+      console.log(results)
+      const saveData = { review_id, star, description, photo }
+      
+      res.status(201).send(saveData);
+      } catch (err) {
+      next(err);
+      }
+  });
+
+  reviewRouter.post("/stores/:store_id/:review_id/얀like", async (req, res, next) => {
+    try {//로그인 돼있나 확인하는 코드 나중에 추가예정
+        //로그인된 유저를 토큰 값으로 확인한후 user_id를 받아오기
+        // const user_id = req.currentUserId;
+        const user_id = 2;
+        const review_id = req.params.review_id;
+        const tag = 2;
+  
+        //db에 존재하는 것 삭제
+        const [result, field, error] = await pool.query(
+          // `select count(user_id) from like_reviews group by review_id, having count(review_id)>1`
+          `DELETE FROM reviews WHERE review_id=${review_id} and user_id=${user_id}`
+        )
+        if (error) throw error;
+
+        // db에 저장
+        const [results, fields, err] = await pool.query(
+          `INSERT INTO like_reviews user_id = ${user_id}, review_id = ${review_id}, tag = ${tag})`);
+        if (err) throw error;
+  
+        console.log(results)
+        const saveData = { review_id, star, description, photo }
+        
+        res.status(201).send(saveData);
+        } catch (err) {
+        next(err);
+        }
+    });
+
+
+
+
 
 //api index
-reviewRouter.post("/stores/:store_id/review", addReview)
-reviewRouter.get("/stores/:store_id/reviews", storeReviews)
+// reviewRouter.post("/stores/:store_id/review", addReview);
+// reviewRouter.get("/stores/:store_id/reviews", storeReviews);
+// reviewRouter.get("/user/reviewList", userReviews);
+// reviewRouter.put("/review/:review_id", putReview);
+// reviewRouter.delete("/stores/:store_id/:review_id", deleteReview) ;
 
 module.exports = reviewRouter;
 
