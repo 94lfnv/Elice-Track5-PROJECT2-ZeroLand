1ccde8dbff9fc609a02f3c11b652e50419fdfcf6
박 은정 <scpark6021@gmail.com>
2022-10-17 16:51:18 +0900

    [fix]: review 코드리뷰 수정

--- back/src/routers/myPageRouter.js
+++ back/src/routers/myPageRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { pool } = require("../db/database");
+const login_required = require("../middlewares/login_required")
 
 const myPageRouter = express.Router();
 
@@ -11,17 +12,17 @@ myPageRouter.get("/user/reviewList", async (req, res, next) => {
       const user_id = 1
   
     let [results, fields, error] = await pool.query(
-      `SELECT () FROM reviews WHERE reviews.user_id = ${user_id}`
+      `SELECT review_id, star, description, photo, created_time, updated_time FROM reviews WHERE reviews.user_id = ${user_id}`
     );
   
     for (var i = 0; i < results.length; i++) {
       let k = results[i].review_id
       //그 리뷰를 좋아하는 사람 목록
       let [reviewLikeList] = await pool.query(
-        `SELECT * FROM like_reviews WHERE like_reviews.review_id = ${k} and tag=1`)
+        `SELECT like_review_id, created_time, user_id FROM like_reviews WHERE like_reviews.review_id = ${k} and tag="like"`)
       //그 리뷰를 싫어하는 사람 목록
       let [reviewDislikeList] = await pool.query(
-        `SELECT * FROM like_reviews WHERE like_reviews.review_id = ${k} and tag=2`)
+        `SELECT like_review_id, created_time, user_id FROM like_reviews WHERE like_reviews.review_id = ${k} and tag="dislike"`)
       results[i].like_reviews = reviewLikeList
       results[i].dike_reviews = reviewDislikeList
     }
