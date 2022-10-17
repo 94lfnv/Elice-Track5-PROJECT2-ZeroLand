9b95d732068e0deaf80d6ba6e142e5186a67d87f
Kim Byeongyong <ryankim8815@gmail.com>
2022-10-17 17:01:47 +0900

    [Fix]: login_required.js - deleted 'console.log()'

--- back/src/middlewares/login_required.js
+++ back/src/middlewares/login_required.js
@@ -4,7 +4,6 @@ const { pool } = require("../db/database");
 const login_required = async function (req, res, next) {
   // request 헤더로부터 authorization bearer 토큰을 받음.
   const userToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
-  console.log("FE에서 로그인 후 이동할 때 준 userToken: ", userToken);
   // 이 토큰은 jwt 토큰 문자열이거나, 혹은 "null" 문자열임.
   // 토큰이 "null" 일 경우, login_required 가 필요한 서비스 사용을 제한함.
   if (userToken === "null") {
