87f46584c4eecb1d8d59640386440e4f47968088
Kim Byeongyong <ryankim8815@gmail.com>
2022-10-18 10:25:35 +0900

    [Feat]: (GET)userRouter.js - added email & nickname checking api

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -26,6 +26,56 @@ const userList = async (req, res, next) => {
     next(err);
   }
 };
+
+// GET: 회원가입 - Email 중복 확인 기능
+const userRegisterEmail = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+
+    // 이메일 중복 확인
+    const [res_checkID, fld_checkID, err_checkID] = await pool.query({
+      sql: "SELECT * FROM users WHERE `email` = ? ",
+      values: [email],
+    });
+    if (err_checkID) throw err_checkID;
+    else if (JSON.stringify(res_checkID) !== "[]") {
+      res.status(200).json({
+        result: false,
+        errorMessage:
+          "입력하신 email로 가입된 내역이 있습니다. 다시 한 번 확인해 주세요.",
+        errorCause: "email",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET: 회원가입 - nickname 중복 확인 기능
+const userRegisterNickname = async (req, res, next) => {
+  try {
+    const nickname = req.body.nickname;
+
+    // 닉네임 중복 확인
+    const [res_checkNickname, fld_checkNickname, err_checkNickname] =
+      await pool.query({
+        sql: "SELECT * FROM users WHERE `nickname` = ? ",
+        values: [nickname],
+      });
+    if (err_checkNickname) throw err_checkNickname;
+    else if (JSON.stringify(res_checkNickname) !== "[]") {
+      res.status(200).json({
+        result: false,
+        errorMessage:
+          "입력하신 닉네임으로 가입된 내역이 있습니다. 다시 한 번 확인해 주세요.",
+        errorCause: "nickname",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
 // POST: 회원가입 기능
 const userRegister = async (req, res, next) => {
   try {
@@ -63,22 +113,27 @@ const userRegister = async (req, res, next) => {
         errorCause: "nickname",
       });
     }
-    // 비밀번호 해쉬화
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // db에 저장
-    const [res_save, fld_save, err_save] = await pool.query({
-      sql: "INSERT INTO users (email, password, nickname) VALUES (?, ?, ?)",
-      values: [email, hashedPassword, nickname],
-    });
-    if (err_save) throw err_save;
-    // const [res_new, fld_new, err_new] = await pool.query({
-    //   sql: "SELECT * FROM users WHERE `email` = ? ",
-    //   values: [email],
-    // });
-    // if (err_new) throw err_new;
-    // res.status(200).json(res_new[0]);
-    const res_success = "회원가입이 성공적으로 이뤄졌습니다.";
-    res.status(200).send(res_success);
+    if (
+      JSON.stringify(res_checkID) === "[]" &&
+      JSON.stringify(res_checkNickname) === "[]"
+    ) {
+      // 비밀번호 해쉬화
+      const hashedPassword = await bcrypt.hash(password, 10);
+      // db에 저장
+      const [res_save, fld_save, err_save] = await pool.query({
+        sql: "INSERT INTO users (email, password, nickname) VALUES (?, ?, ?)",
+        values: [email, hashedPassword, nickname],
+      });
+      if (err_save) throw err_save;
+      // const [res_new, fld_new, err_new] = await pool.query({
+      //   sql: "SELECT * FROM users WHERE `email` = ? ",
+      //   values: [email],
+      // });
+      // if (err_new) throw err_new;
+      // res.status(200).json(res_new[0]);
+      const res_success = "회원가입이 성공적으로 이뤄졌습니다.";
+      res.status(200).send(res_success);
+    }
   } catch (err) {
     next(err);
   }
@@ -310,6 +365,11 @@ const userDelete = async function (req, res, next) {
 
 // api index
 userAuthRouter.get("/userlist", asyncHandler(userList));
+userAuthRouter.get("/user/register/email", asyncHandler(userRegisterEmail));
+userAuthRouter.get(
+  "/user/register/nickname",
+  asyncHandler(userRegisterNickname)
+);
 userAuthRouter.post("/user/register", asyncHandler(userRegister));
 userAuthRouter.post("/user/login", asyncHandler(userLogin));
 userAuthRouter.get(
@@ -340,49 +400,3 @@ userAuthRouter.delete(
 );
 
 module.exports = userAuthRouter;
-
-//-------------options-start--------------//
-
-// // ex) 유저리스트 확인 기능
-// // option 1. 코드하나로 해결
-// userAuthRouter.get("/userlist", async (req, res, next) => {
-//   try {
-//     const [results, fields, error] = await pool.query("SELECT * FROM users");
-//     if (error) throw error;
-//     // console.log(results);
-//     // console.log(fields);
-
-//     res.status(200).send(JSON.stringify(results));
-//   } catch (err) {
-//     next(err);
-//   }
-// }
-
-// // option 2. 코드 두개
-// const userList = async (req, res, next) => {
-//   try {
-//     const [results, fields, error] = await pool.query("SELECT * FROM users");
-//     if (error) throw error;
-//     // console.log(results);
-//     // console.log(fields);
-
-//     res.status(200).send(JSON.stringify(results));
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// // userAuthRouter.get("/userlist", userList);
-
-// // option 3. 코드 파일 분리
-// // Error: Callback function is not available with promise clients.
-// userAuthRouter.get("/userlist", async (req, res, next) => {
-//   try {
-//     const [results, fields, error] = await userAuthService.getUsers();
-//     res.status(200).json(results);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-////////////////
-// module.exports = userAuthRouter;
-//-------------options-end--------------//
