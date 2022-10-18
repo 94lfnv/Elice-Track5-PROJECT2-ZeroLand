f5a86861741c1e677170fe8b0cb90f3a4b2c0eca
박 은정 <scpark6021@gmail.com>
2022-10-18 17:16:49 +0900

    [feat]: 연령대별 환경관심정도 데이터 api

--- back/src/routers/introRouter.js
+++ back/src/routers/introRouter.js
@@ -35,5 +35,37 @@ const introData = async (req, res, next) => {
   }
 };
 
+// GET: 연령대별 환경관심정도 데이터
+const introData2 = async (req, res, next) => {
+  try {
+    const file_name = "국민환경의식조사원자료.csv";
+    const csvPath = path.join(__dirname, "..", "data", file_name);
+    const csv = readFileSync(csvPath, "utf-8");
+    // 문자열을 줄바꿈으로 구분 => 배열에 저장
+    const rows = csv.split("\r\n");
+    // 빈 배열 생성: CSV의 각 행을 담을 JSON 객체임
+    const jsonArray = [];
+    // 제목 행 추출 후, 콤마로 구분 => 배열에 저장
+    const header = rows[0].split(",");
+    // 내용 행 전체를 객체로 만들어, jsonArray에 담기
+    for (let i = 1; i < rows.length; i++) {
+      // 빈 객체 생성: 각 내용 행을 객체로 만들어 담아둘 객체임
+      let obj = {};
+      // 각 내용 행을 콤마로 구분
+      let row = rows[i].split(",");
+      // 각 내용행을 {제목1:내용1, 제목2:내용2, ...} 형태의 객체로 생성
+      for (let j = 0; j < header.length; j++) {
+        obj[header[j]] = row[j];
+      }
+      // 각 내용 행의 객체를 jsonArray배열에 담기
+      jsonArray.push(obj);
+    }
+    res.status(200).json(jsonArray);
+  } catch (err) {
+    next(err);
+  }
+};
+
 introRouter.get("/intro", asyncHandler(introData));
+introRouter.get("/intro/2", asyncHandler(introData2));
 module.exports = introRouter;
