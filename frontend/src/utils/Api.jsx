4a8ba437adc4bbec26f10066a353c5e08f9427fa
94lfnv <94lfnv@gmail.com>
2022-10-18 13:41:41 +0900

    Fix: register

--- frontend/src/utils/Api.jsx
+++ frontend/src/utils/Api.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 axios.defaults.timeout = 3000;
+axios.defaults.headers["Content-Type"] = "application/json";
 const backendPortNumber = import.meta.env.VITE_BACK_PORT_NUM;
 const BASE_URL = `http://${window.location.hostname}:${backendPortNumber}/`;
 
-
 axios.interceptors.response.use(
     (res) => {
-      return res;
+      return res.data;
     },
     (err) => {
       console.log(err);
@@ -15,79 +15,42 @@ axios.interceptors.response.use(
     }
   );
 
+// axios.interceptors.request.use(
+//     (req) => {
+//         console.log(req.headers, req.body);
+//         // express로 보내는 데이터 타입이 "이미지 형태"일 경우, 
+//         // "Content-Type = multipart/form-data";
+//     },
+//     (err) => {
+//         console.log(err);
+//     }
+// )
+
+const customAxios = axios.create({
+    headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
+    }
+  });
+
 const post = async (endpoint, data) => {
     const bodyData = JSON.stringify(data);
     // bodydata는 백엔에서 처리되기 전 데이터
-    console.log(bodyData);
+    // console.log(bodyData);
 
-    return axios.post(BASE_URL + endpoint, bodyData, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-        } });
+    return customAxios.post(BASE_URL + endpoint, bodyData);
 }
 
 const get = async (endpoint, params="") => {
-    console.log("get 요청");
-    console.log(sessionStorage.getItem("userToken"));
-    return axios.get(BASE_URL + endpoint + "/" + params, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-        } });
+    return customAxios.get(BASE_URL + endpoint + "/" + params);
 }
 
 const put = async (endpoint, data) => {
     const bodyData = JSON.stringify(data);
-    return axios.put(BASE_URL + endpoint, bodyData, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-        } });
+    return customAxios.put(BASE_URL + endpoint, bodyData);
 }
 
 const del = async (endpoint, params="") => {
-    // const bodyData = JSON.stringify(data);
-    // console.log(bodyData);
-    return axios.delete(BASE_URL + endpoint + "/" + params, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
-        } });
+    return customAxios.delete(BASE_URL + endpoint + "/" + params);
 }
 
 export { post, get, put, del as delete };
-
-// export const api = axios.create({
-//     baseURL: BASE_URL,
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     timeout: 3000,
-// });
-
-// api.interceptors.request.use(
-//     (req) => {
-//       return req;
-//     },
-//     (error) => {
-//       throw new Error(`This is error! ${error}`);
-//     }
-//   );
-  
-//  api.interceptors.response.use(
-//     (res) => {
-//       return res;
-//     },
-//     (error) => {
-//       if (error.response.status >= 400 && error.response.status < 500) {
-//         console.log(error);
-//         return Promise.reject(error);
-//         // throw new Error(`This is request error!`);
-//       }
-//       if (error.response.status >= 500) {
-//         console.log(error);
-//         throw new Error("This is server error!");
-//       }
-//     }
-//   );
