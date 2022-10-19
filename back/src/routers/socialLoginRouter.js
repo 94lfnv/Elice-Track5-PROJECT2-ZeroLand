e0eae5fbbcd747f6a0120fa45935f075bbf5a07b
Kim Byeongyong <ryankim8815@gmail.com>
2022-10-19 19:38:59 +0900

    [Fix]: socialLoginRouter.js - KAKAO can get user email

--- back/src/routers/socialLoginRouter.js
+++ back/src/routers/socialLoginRouter.js
@@ -15,23 +15,30 @@ axios.interceptors.response.use(
     throw new Error("(!) axios error");
   }
 );
+const makeFormData = (params) => {
+  const searchParams = new URLSearchParams();
+  Object.keys(params).forEach((key) => {
+    searchParams.append(key, params[key]);
+  });
+  return searchParams;
+};
 
 ////////////////////////////////////////
 /////////////  네  이  버  ///////////////
 ////////////////////////////////////////
-const naver = async (req, res, next) => {
-  // const userToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
-  // const header = "Bearer " + userToken;
+const naverReqToken = async (req, res, next) => {
+  const code = req.body.code;
+  const state = req.body.state;
   const client_id = "JRa7NrbtcesvuNyjkj6I";
   const client_secret = "wKlBmjAspw";
   const redirectURI = encodeURI("http://127.0.0.1:5173/");
-  //   const code = req.headers["authorization"]?.split(" ")[1] ?? "null";
-  const code = req.headers["authorization"]?.split(" ")[1] ?? "null";
-  //   const state = 200;
-  const state = "77ee175d-e7a3-42eb-8aea-382a4e02a953";
   try {
-    // const apiURL = "https://openapi.naver.com/v1/nid/me";
-    // const code = req.query.code;
+    // const makeFormData = (params) => {
+    //   const searchParams = new URLSearchParams();
+    //   Object.keys(params).forEach((key) => {
+    //     searchParams.append(key, params[key]);
+    //   });
+    // };
     const api_url =
       //   "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=" +
       "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=" +
@@ -60,8 +67,29 @@ const naver = async (req, res, next) => {
         console.log("error = " + response.statusCode);
       }
     });
+    /////////
+    let naverToken = "";
+    await axios({
+      method: "POST",
+      headers: {
+        "content-type": "text/json;charset=utf-8",
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret,
+      },
+      url: api_url,
+      // data: makeFormData({
+      //   grant_type: "authorization_code",
+      //   client_id: REST_API_KEY,
+      //   redirect_uri: REDIRECT_URI,
+      //   code: code,
+      // }),
+    }).then((res) => {
+      naverToken = res;
+    });
+    console.log("naverToken: ", naverToken);
+
     // const user = await naverService.getToken({ code });
-    // res.status(200).json(user);
+    res.status(200).json(naverToken);
   } catch (error) {
     next(error);
   }
@@ -70,45 +98,20 @@ const naver = async (req, res, next) => {
 ////////////////////////////////////////
 /////////////  카  카  오  ///////////////
 ////////////////////////////////////////
-const kakaoReqCode = async (req, res, next) => {
-  const REST_API_KEY = "9fd6d9d615c25ff01b60a3a988e942bc";
-  const client_secret = "JvbCFkI8Lfxq9hkwMA9vbPOk9c52edrJ";
-  //   const REDIRECT_URI = encodeURI("127.0.0.1:5173/login/oauth2/code/kakao");
-  const REDIRECT_URI = "http://127.0.0.1:5173/login/oauth2/code/kakao";
-  try {
-    const api_url = `kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-    const options = {
-      url: api_url,
-      headers: {},
-    };
-    // <FE에서 리디렉션 url를 추적 가능한 경우>
-    console.log("options: ", options);
-    res.status(200).json({
-      result: true,
-      resultMessage:
-        "아래 REDIRECT_URI로 이동하시어 사용자에게 동의를 받아주시고, url로 넘어오는 code값을 담아서 api 요청을 해주세요.",
-      REDIRECT_URI: api_url,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-////////////////////////////////////
-///// 토큰 받는 것 까지 정상 작동////
+//////// 토큰 받는 것 까지 정상 작동//////////
+// "REDIRECT_URI": "kauth.kakao.com/oauth/authorize?client_id=9fd6d9d615c25ff01b60a3a988e942bc&redirect_uri=http://127.0.0.1:5173/login/oauth2/code/kakao&response_type=code"
 const kakaoReqToken = async (req, res, next) => {
   const code = req.body.code;
   const REST_API_KEY = "9fd6d9d615c25ff01b60a3a988e942bc";
-  const client_secret = "JvbCFkI8Lfxq9hkwMA9vbPOk9c52edrJ";
   const REDIRECT_URI = "http://127.0.0.1:5173/login/oauth2/code/kakao";
   try {
-    const makeFormData = (params) => {
-      const searchParams = new URLSearchParams();
-      Object.keys(params).forEach((key) => {
-        searchParams.append(key, params[key]);
-      });
-      return searchParams;
-    };
+    // const makeFormData = (params) => {
+    //   const searchParams = new URLSearchParams();
+    //   Object.keys(params).forEach((key) => {
+    //     searchParams.append(key, params[key]);
+    //   });
+    //   return searchParams;
+    // };
     let kakaoToken = "";
     await axios({
       method: "POST",
@@ -129,33 +132,47 @@ const kakaoReqToken = async (req, res, next) => {
       .catch((err) => {
         console.log(err);
       });
-    console.log("kakaoToken: ", kakaoToken);
+    // console.log("kakaoToken: ", kakaoToken);
+    // const kakaoAccessToken = kakaoToken.access_token;
+    // console.log("kakaoAccessToken: ", kakaoAccessToken);
+    const kakaoAccessToken = JSON.stringify(kakaoToken.access_token);
+    // console.log("kakaoAccessToken_JSON.stringify: ", kakaoAccessToken);
 
     let kakaoUser = "";
     ///////정보 받아오기///////
+    const access_token = kakaoAccessToken;
+    // console.log("access_token: ", access_token);
     await axios({
-      method: "POST",
+      //   method: "POST",
+      method: "GET",
       headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        // "content-type": "application/x-www-form-urlencoded;charset=utf-8",
         Authorization: `bearer ${access_token}`,
       },
-      url: "https://kauth.kakao.com/v2/user/me",
-      data: makeFormData({
-        grant_type: "authorization_code",
-        secure_resource: false,
-        property_keys: ["kakao_account.email"],
-        // property_keys: ["account_email"],
-      }),
+      //   url: "https://kapi.kakao.com/v2/user/me",
+      url: "https://kapi.kakao.com/v1/oidc/userinfo",
+      //   data: makeFormData({
+      //     grant_type: "authorization_code",
+      //     secure_resource: false,
+      //     property_keys: ["kakao_account.email"],
+      //     // property_keys: ["account_email"],
+      //   }),
     })
       .then((res) => {
-        console.log(res);
+        console.log("res_raw: ", res);
         kakaoUser = res;
       })
       .catch((err) => {
         console.log(err);
       });
-    console.log(kakaoUser);
-    res.status(200).json(kakaoUser);
+    console.log("before_JSON.stringify: ", kakaoUser.email);
+    const kakaoUserEmailWL = JSON.stringify(kakaoUser.email);
+    console.log(kakaoUserEmailWL);
+    console.log("before replace: ", typeof kakaoUserEmailWL); // before_JSON.stringify:  new_peridot@naver.com
+    const kakaoUserEmail = kakaoUserEmailWL.replace('\\"', "");
+    console.log("after replace: ", kakaoUserEmail); // after replace:  "new_peridot@naver.com"
+
+    res.status(200).json(kakaoUserEmail);
   } catch (error) {
     next(error);
   }
@@ -165,7 +182,6 @@ const kakaoReqUserInfo = async (req, res, next) => {
   const access_token = req.body.access_token;
   //   const code = "q9oXFfantmUUdYBKTI70kT2jRAH57gIojFcSPIGNGQs8pgUj0utB9JTHkW205aOJmuw_TQopb9UAAAGD7rfq3g";
   const REST_API_KEY = "9fd6d9d615c25ff01b60a3a988e942bc";
-  const client_secret = "JvbCFkI8Lfxq9hkwMA9vbPOk9c52edrJ";
   //   const REDIRECT_URI = encodeURI("127.0.0.1:5173/login/oauth2/code/kakao");
   const REDIRECT_URI = "http://127.0.0.1:5173/login/oauth2/code/kakao";
   try {
@@ -217,8 +233,7 @@ const kakaoReqUserInfo = async (req, res, next) => {
     next(error);
   }
 };
-socialLoginRouter.get("/naver", asyncHandler(naver));
-socialLoginRouter.get("/kakaoReqCode", asyncHandler(kakaoReqCode));
+socialLoginRouter.post("/naverReqToken", asyncHandler(naverReqToken));
 socialLoginRouter.post("/kakaoReqToken", asyncHandler(kakaoReqToken));
 socialLoginRouter.post("/kakaoReqUserInfo", asyncHandler(kakaoReqUserInfo));
 module.exports = socialLoginRouter;
