0d721c6638895406e630a2fa5804d1dc17269118
haleychoi <chldudgus617@gmail.com>
2022-10-20 17:31:26 +0900

    Fix: delete unused imported library

--- frontend/src/components/Map/StoreLike.jsx
+++ frontend/src/components/Map/StoreLike.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import { Col } from "react-bootstrap";
 import * as Api from "../../utils/Api";
 
